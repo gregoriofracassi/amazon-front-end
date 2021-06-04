08233faed6034984374cb610ec249b0416223d07
MaLIE2018 <liebsch@pm.me>
2021-06-04 17:39:30 +0200

    search

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,32 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../Header.css";
 import SearchIcon from "@material-ui/icons/Search";
 import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 import { Link, withRouter } from "react-router-dom";
 
 function Header(props) {
+  const [search, setSearch] = useState("");
   const handleChange = (e) => {
-    const search = props.location.search;
+    // const search = props.location.search;
     if (e.target.value.length > 0 && e.target.value.length) {
       props.onQueryChange(e.target.value);
       if (search) {
-        console.log("props.location.search:", props.location.search);
+        console.log("props.location.search2:", props.location.search);
         props.history.push(search + `&query=${e.target.value}`);
       } else {
-        props.history.push(props.match.path + `?query=${e.target.value}`);
+        props.history.push(
+          props.match.path + `products?query=${e.target.value}`
+        );
       }
     } else {
       props.history.push(props.match.path);
       props.onQueryChange(e.target.value);
     }
   };
+  useEffect(() => {
+    if (
+      !props.location.search.startsWith("?query") &&
+      !props.location.search.includes("query")
+    ) {
+      console.log("Tes");
+      setSearch(props.location.search);
+    }
+  }, [props.location.search]);
 
   return (
     <div className='header'>
-      <img
-        className='header__logo'
-        src='http://pngimg.com/uploads/amazon/amazon_PNG11.png'
-      />
+      <Link to='/'>
+        <img
+          className='header__logo'
+          src='http://pngimg.com/uploads/amazon/amazon_PNG11.png'
+        />
+      </Link>
 
       <div className='header__search'>
         <input

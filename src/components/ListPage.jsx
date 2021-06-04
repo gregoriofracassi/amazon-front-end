dadd2f445370ea75f744bdf001b2bf9b6dfa09eb
MaLIE2018 <liebsch@pm.me>
2021-06-04 15:31:52 +0200

    pagination

--- src/components/ListPage.jsx
+++ src/components/ListPage.jsx
@@ -1,58 +1,55 @@
-import React from "react"
-import { Container, Row, Col } from "react-bootstrap"
-
+import React from "react";
+import { Container, Row, Col, Pagination } from "react-bootstrap";
+import { Link, withRouter } from "react-router-dom";
 class ListPage extends React.Component {
-  state = {
-    products: [],
-  }
-
-  componentDidMount = async () => {
-    try {
-      let response = await fetch("http://localhost:3001/products")
-
-      if (response.ok) {
-        let data = await response.json()
-        console.log(data)
-        this.setState({ products: data })
-        console.log(this.state.products)
-      }
-    } catch (error) {
-      console.log(error)
-    }
-  }
-
   render() {
+    const count = this.props.products.length;
+    const items = [];
+    let active = 2;
+
+    for (let number = 1; number <= count / 6; number++) {
+      items.push(
+        <Pagination.Item
+          key={number}
+          active={number === active}
+          as={Link}
+          to='/products/'>
+          {number}
+        </Pagination.Item>
+      );
+    }
+
     return (
-      <Container className="mt-4">
+      <Container className='mt-4'>
         <Row>
           <Col xs={2}></Col>
           <Col xs={10}>
-            {this.state.products.map((p) => {
+            {this.props.products.map((p) => {
               return (
                 <Container
                   key={p._id}
-                  className="pointer"
-                  onClick={() => this.props.history.push(`/product/${p._id}`)}
-                >
+                  className='pointer'
+                  onClick={() => this.props.history.push(`/product/${p._id}`)}>
                   <Row>
                     <Col xs={2}>
-                      <img className="img-fluid" src={p.imageUrl} alt="" />
+                      <img className='img-fluid' src={p.imageUrl} alt='' />
                     </Col>
                     <Col xs={10}>
                       <h5>{p.name}</h5>
                       <p>{p.description}</p>
                       <h4>{p.price}</h4>
                     </Col>
-                    <hr className="my-3"></hr>
+                    <hr className='my-3'></hr>
                   </Row>
                 </Container>
-              )
+              );
             })}
           </Col>
         </Row>
+        <Pagination>{items}</Pagination>
       </Container>
-    )
+    );
   }
 }
 
-export default ListPage
+export default withRouter(ListPage);
